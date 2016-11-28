075b6163d2dc131dd7d9bd39129574ab327739f8
qiyiping <qiyiping@meituan.com>
2016-11-28 10:21:55 +0800

    update evaluate.py

--- evaluation.py
+++ evaluation.py
@@ -7,19 +7,16 @@ import math
 import numpy as np
 
 def evaluate(db, model):
-    directions = [(False, False), (False, True), (True, False), (True, True)]
-
     s = 0.0
     n = 0.0
-    for d1,d2 in directions:
-        for moves, result in db.xgames(d1, d2):
-            if np.random.rand() > 0.1:
-                continue
-            b = Board()
-            for p, r, c in moves:
-                b.flip(r, c, p)
-                s += (model(b) - result) * (model(b) - result)
-                n += 1.0
+    for moves, result in db.games:
+        if np.random.rand() > 0.1:
+            continue
+        b = Board()
+        for p, r, c in moves:
+            b.flip(r, c, p)
+            s += (model(b) - result) * (model(b) - result)
+            n += 1.0
     return (n, math.sqrt(s/n))
 
 if __name__ == '__main__':
